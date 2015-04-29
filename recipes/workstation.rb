a2e537993de91a46078fe83e43c6fcb3b9652821
Marcus <marcus.beach@ascendumims.com>
2015-04-28 22:59:17 -0400

    Syntax corrections

--- recipes/workstation.rb
+++ recipes/workstation.rb
@@ -13,19 +13,21 @@ with_driver "docker"
 machine_image 'workstation' do
   recipe 'chef-dk::default'
 
-  machine_options :docker_option =>
+  machine_options :docker_option => {
   :base_image => {
      :name => 'ubuntu',
      :repository => 'ubuntu',
      :tag => '14.04'
+   }
  }
-}
 end
+
 machine_batch do
   1.upto(node['provisioner']['workstation_number']) do |i|
     machine "workstation#{i}" do
       from_image 'workstation'
-      machine_options :docker_options => {
+
+      machine_options :bootstrap_options => {
         :convergence_options => {
           :ssl_verify_mode => 'verify_none'
         }
