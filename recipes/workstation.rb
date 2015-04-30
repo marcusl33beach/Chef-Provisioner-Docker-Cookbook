6e2fbace41c9ec17b37e6dbd8ccd8643e9ad8d2f
Marcus <marcus.beach@ascendumims.com>
2015-04-29 20:49:34 -0400

    More debugging.

--- recipes/workstation.rb
+++ recipes/workstation.rb
@@ -9,16 +9,16 @@ require "chef/provisioning/docker_driver"
 with_driver "docker"
 
 machine 'Workstation' do
-    recipe 'chef-df::default'
+    recipe 'chef-dk::default'
 
     machine_options :docker_options => {
       :base_image => {
           :name => 'ubuntu',
           :repository => 'ubuntu',
           :tag => '14.04'
-      },
-   :convergence_options => {
-     :ssl_verify_mode => 'verify_none'
-   }
- }
+      }
+    },
+    :convergenc_options => {
+      :ssl_verify_mode => 'verify_none'
+    }
 end
